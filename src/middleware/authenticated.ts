e3fd26d25359de5ef78e1b33a2eea9aa87fea0f3
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2019-07-17 16:01:03 +0900

    [wip] add pages

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -3,18 +3,19 @@ import { auth } from '@/lib/firebase'
 
 export default (context: Context) => {
   // StoreにユーザデータがあればOK
-  if (context.store.getters.isAuthenticated) return
-
-  // Storeにユーザデータがなければ、Firebase Authenticationをチェック(ローディング画面を表示)
-  context.store.dispatch('user/startLoading')
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      context.store.dispatch('user/signin', user)
-    } else {
-      context.redirect('/')
-    }
-    setTimeout(() => {
+  if (!context.store.getters.isAuthenticated) {
+    // Storeにユーザデータがなければ、Firebase Authenticationをチェック(ローディング画面を表示)
+    context.store.dispatch('user/startLoading')
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        context.store.dispatch('user/signin', user)
+      } else {
+        context.redirect('/')
+      }
       context.store.dispatch('user/endLoading')
-    }, 1000)
-  })
+      // setTimeout(() => {
+      //   context.store.dispatch('user/endLoading')
+      // }, 1000)
+    })
+  }
 }
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,30 +1,24 @@
 <template lang="pug">
-  .mypage.h-screen.bg-gray-lightest.overflow-auto.flex.items-center
-    .mypage__content.w-full.px-6
+  .home.h-screen.bg-gray-lightest.overflow-auto.flex.items-center
+    .home__content.w-full.px-6
       logo.w-48.m-auto(color="#828282")
-      .mypage__content
+      .home__content
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { UserProfileEntity } from '@/entities/User'
 import BaseButton from '@/components/atoms/BaseButton.vue'
-import FormLargeLabel from '@/components/atoms/FormLargeLabel.vue'
 import Logo from '@/components/atoms/Logo.vue'
-import UserProfileForm from '@/components/molecules/UserProfileForm.vue'
-import UserPreferenceForm from '@/components/molecules/UserPreferenceForm.vue'
 import { userModule } from '@/store/user/const'
 
 @Component({
   components: {
     BaseButton,
-    FormLargeLabel,
-    Logo,
-    UserProfileForm,
-    UserPreferenceForm
+    Logo
   }
 })
-export default class Mypage extends Vue {
+export default class Home extends Vue {
   @userModule.State('profile')
   private profile!: UserProfileEntity
 }
