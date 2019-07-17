64d53f0b112908c4692c8d5b2afc8c0eb022ecfb
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2019-07-17 20:59:58 +0900

    fix authenticated middleware

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -3,19 +3,19 @@ import { auth } from '@/lib/firebase'
 
 export default (context: Context) => {
   // StoreにユーザデータがあればOK
-  if (!context.store.getters.isAuthenticated) {
-    // Storeにユーザデータがなければ、Firebase Authenticationをチェック(ローディング画面を表示)
-    context.store.dispatch('user/startLoading')
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        context.store.dispatch('user/signin', user)
-      } else {
-        context.redirect('/')
-      }
-      context.store.dispatch('user/endLoading')
-      // setTimeout(() => {
-      //   context.store.dispatch('user/endLoading')
-      // }, 1000)
-    })
-  }
+  if (context.store.getters['user/isAuthenticated']) return
+
+  // Storeにユーザデータがなければ、Firebase Authenticationをチェック(ローディング画面を表示)
+  context.store.dispatch('user/startLoading')
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      context.store.dispatch('user/signin', user)
+    } else {
+      context.redirect('/')
+    }
+    context.store.dispatch('user/endLoading')
+    // setTimeout(() => {
+    //   context.store.dispatch('user/endLoading')
+    // }, 1000)
+  })
 }
