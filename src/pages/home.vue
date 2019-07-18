e504bbf75c903f29f823521b7c7f7420a4923415
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2019-07-18 12:28:13 +0900

    clean home & request page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,25 +1,11 @@
 <template lang="pug">
-  .home.h-screen.bg-gray-lightest.overflow-auto.flex.items-center
+  .home
     .home__content.w-full.px-6
-      logo.w-48.m-auto(color="#828282")
-      .home__content
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { UserProfileEntity } from '@/entities/User'
-import BaseButton from '@/components/atoms/BaseButton.vue'
-import Logo from '@/components/atoms/Logo.vue'
-import { userModule } from '@/store/user/const'
 
-@Component({
-  components: {
-    BaseButton,
-    Logo
-  }
-})
-export default class Home extends Vue {
-  @userModule.State('profile')
-  private profile!: UserProfileEntity
-}
+@Component({})
+export default class Home extends Vue {}
 </script>
